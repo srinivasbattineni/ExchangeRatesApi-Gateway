c62839fe5f3b229c873b55dcf1feab0bdd012a7f
srinivas <srinivasbattineni9@gmail.com>
2021-11-05 13:48:14 +0530

    First commit

---
+++ src/main/java/com/exchangeratesgateway/ExchangeRatesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.exchangeratesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRatesApiGatewayApplication.class, args);
+	}
+
+}
